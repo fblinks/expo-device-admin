056f3f4073006669a08a5f5de176961a4cb362aa
Fabrizio Bertone <52320328+fblinks@users.noreply.github.com>
2025-02-25 10:12:27 +0100

    Update MinimalDeviceAdminReceiver.kt

--- android/src/main/java/expo/modules/deviceadmin/MinimalDeviceAdminReceiver.kt
+++ android/src/main/java/expo/modules/deviceadmin/MinimalDeviceAdminReceiver.kt
@@ -4,6 +4,7 @@ import android.app.admin.DeviceAdminReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 
 class MinimalDeviceAdminReceiver : DeviceAdminReceiver() {
     // Optional: Override methods if needed for receiving events
@@ -11,10 +12,12 @@ class MinimalDeviceAdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context, intent: Intent) {
         super.onEnabled(context, intent)
         Log.d("MyDeviceAdminReceiver", "Device admin enabled")
+        Toast.makeText(context, "Device Admin: enabled", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
         super.onDisabled(context, intent)
         Log.d("MyDeviceAdminReceiver", "Device admin disabled")
+        Toast.makeText(context, "Device Admin: disabled", Toast.LENGTH_SHORT).show()
     }
 }
