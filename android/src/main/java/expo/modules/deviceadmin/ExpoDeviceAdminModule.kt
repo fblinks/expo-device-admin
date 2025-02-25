e033b05fbf2b57eab647ed4d44fcd141dca93658
Fabrizio Bertone <52320328+fblinks@users.noreply.github.com>
2025-02-25 10:20:29 +0100

    Update ExpoDeviceAdminModule.kt

--- android/src/main/java/expo/modules/deviceadmin/ExpoDeviceAdminModule.kt
+++ android/src/main/java/expo/modules/deviceadmin/ExpoDeviceAdminModule.kt
@@ -1,31 +1,38 @@
 package expo.modules.deviceadmin
 
+import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import java.net.URL
 import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
 
 class ExpoDeviceAdminModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoDeviceAdmin')` in JavaScript.
     Name("ExpoDeviceAdmin")
 
-    fun rebootDevice(context: Context) {
-    try {
-        val dpm = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
-        val componentName = ComponentName(context, MinimalDeviceAdminReceiver::class.java)
+    // Existing function to reboot the device.
+    Function("rebootDevice") { promise: Promise ->
+      try {
+        val dpm = appContext.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val componentName = ComponentName(appContext, MinimalDeviceAdminReceiver::class.java)
         dpm.reboot(componentName)
-    } catch (e: Exception) {
-        e.printStackTrace()
+        promise.resolve(null)
+      } catch (e: Exception) {
+        promise.reject("REBOOT_ERROR", e)
+      }
     }
-}
 
+    // New function to set lock task features.
+    Function("setLockTaskFeatures") { features: Int, promise: Promise ->
+      try {
+        val dpm = appContext.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val componentName = ComponentName(appContext, MinimalDeviceAdminReceiver::class.java)
+        dpm.setLockTaskFeatures(componentName, features)
+        promise.resolve(null)
+      } catch (e: Exception) {
+        promise.reject("SET_FEATURES_ERROR", e)
+      }
+    }
   }
 }
