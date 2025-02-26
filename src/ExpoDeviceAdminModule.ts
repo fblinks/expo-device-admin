9889718be51e21cd5ee9c0fde3d498d22214c720
Fabrizio Bertone <52320328+fblinks@users.noreply.github.com>
2025-02-26 09:44:36 +0100

    Update ExpoDeviceAdminModule.ts

--- src/ExpoDeviceAdminModule.ts
+++ src/ExpoDeviceAdminModule.ts
@@ -1,14 +1,12 @@
-import { NativeModule, requireNativeModule } from "expo";
+import { NativeModule, requireNativeModule } from "expo-modules-core";
 
 declare class ExpoDeviceAdminModule extends NativeModule {
   rebootDevice(): Promise<void>;
   setLockTaskFeatures(features: number): Promise<void>;
   lockEverythingExceptPowerButton(): Promise<void>;
-  isDeviceOwner(): Promise<boolean>; // Ensure this matches the native function
+  isDeviceOwner(): Promise<boolean>; // Matches the native function return type
 }
 
-// Load the native module from JSI
 const ExpoDeviceAdmin = requireNativeModule<ExpoDeviceAdminModule>("ExpoDeviceAdmin");
 
 export default ExpoDeviceAdmin;
-
