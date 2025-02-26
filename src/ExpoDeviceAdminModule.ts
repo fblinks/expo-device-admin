ad68757693a2d4dd7255dadb13eb6161f82abe26
Fabrizio Bertone <52320328+fblinks@users.noreply.github.com>
2025-02-26 15:10:56 +0100

    Update ExpoDeviceAdminModule.ts

--- src/ExpoDeviceAdminModule.ts
+++ src/ExpoDeviceAdminModule.ts
@@ -3,7 +3,7 @@ import { NativeModule, requireNativeModule } from "expo-modules-core";
 declare class ExpoDeviceAdminModule extends NativeModule {
   rebootDevice(): Promise<void>;
   setLockTaskFeatures(features: number): Promise<void>;
-  lockEverythingExceptPowerButton(): Promise<void>;
+  enableKioskMode(): Promise<void>;
   isDeviceOwner(): Promise<boolean>; // Matches the native function return type
 }
 
